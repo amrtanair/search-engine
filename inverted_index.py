674e95471930c3b343578f95271a822f8df0907e
Amrita Nair <amritaanair98@gmail.com>
2020-08-30 11:42:37 +0530

    created k dimensional vector for all urls
    messy

--- inverted_index.py
+++ inverted_index.py
@@ -4,6 +4,9 @@ import math
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize
+import numpy as np
+from sklearn import preprocessing
+from sklearn.metrics.pairwise import cosine_similarity
 stop_words = set(stopwords.words('english'))
 
 def fetch_soup(url):
@@ -29,6 +32,11 @@ def tokenize(text):
                 tokens.append(w.lower())
 
     return tokens, document
+
+def normalize(vector):
+    X = np.asarray(vector, dtype=np.float).reshape(-1,1)
+    X_normalized = preprocessing.normalize(X, norm='l2')
+    return X_normalized
 
 def generate_inverted_index(url):
     text = fetch_soup(url)
@@ -58,33 +66,41 @@ def merge_inverted_index(child_inverted_index, mother_inverted_index, df, doc_id
             df[key] = df[key] + 1
     return mother_inverted_index, df
 
-def compute_idf(df, y):
-    for key in df:
-        x = (y+1)/df[key]
-        df[key] = math.log(x) + 1
+def compute_idf(df, size):
+    for key in df.keys():
+        df[key] = math.log((size+1)/df[key]) + 1
     return df
 
 def compute_tfidf(tf, idf):
     tfidf_hash = [] # list of hashes(list of tfidf for every word in every doc)
+    tfidf = []
     for i, doc in enumerate(tf):
         tfidfi = {}
         for key in idf:
-            if key not in doc:
+            if key not in doc.keys():
                 tfidfi[key] = 0
             else:
                 tfidfi[key] = idf[key] * doc[key]
         tfidf_hash.append(tfidfi)
-    return tfidf_hash
+
+    for i in tfidf_hash:
+        k = normalize(list(i.values()))
+        tfidf.append(k)
+    return tfidf
 
 def driver(urls):
     mother_inverted_index = {}
-    df = {}
-    tf = []
+    df = {} # document frequency
+    tf = [] # term frequency
     for i,url in enumerate(urls):
         inverted_index, tfi = generate_inverted_index(url)
         mother_inverted_index, df = merge_inverted_index(inverted_index, mother_inverted_index, df, i)
         tf.append(tfi)
     idf = compute_idf(df, len(urls))
-    tfidf_hash = compute_tfidf(tf, idf)
-    return mother_inverted_index, tfidf_hash
+    tfidf = compute_tfidf(tf, idf)
+
+    map_tfidf_url = {}
+    for (key, url) in zip(tfidf, urls):
+        map_tfidf_url[url] = key
+    return mother_inverted_index, idf, map_tfidf_url
 
--- main.py
+++ main.py
@@ -1,30 +1,45 @@
 # import pdb; pdb.set_trace()
 from inverted_index import *
 
-urls= ['https://harrypotter.fandom.com/wiki/Harry_Potter_and_the_Philosopher%27s_Stone', 
+urls= [
+		'https://harrypotter.fandom.com/wiki/Harry_Potter_and_the_Philosopher%27s_Stone', 
        'https://harrypotter.fandom.com/wiki/Harry_Potter_and_the_Chamber_of_Secrets', 
        'https://harrypotter.fandom.com/wiki/Harry_Potter_and_the_Prisoner_of_Azkaban', 
-       # 'https://harrypotter.fandom.com/wiki/Harry_Potter_and_the_Goblet_of_Fire',
-       # 'https://supernatural.fandom.com/wiki/Dean_Winchester',
-       # 'https://supernatural.fandom.com/wiki/Sam_Winchester',
-       # 'https://www.tutorialspoint.com/ruby-on-rails/rails-scaffolding.htm',
-       # 'https://en.wikipedia.org/wiki/Facebook',
+       'https://harrypotter.fandom.com/wiki/Harry_Potter_and_the_Goblet_of_Fire',
+       'https://supernatural.fandom.com/wiki/Dean_Winchester',
+       'https://supernatural.fandom.com/wiki/Sam_Winchester',
+       'https://www.tutorialspoint.com/ruby-on-rails/rails-scaffolding.htm',
+       'https://en.wikipedia.org/wiki/Facebook',
        'https://en.wikipedia.org/wiki/Anchovy',
-       'https://en.wikipedia.org/wiki/Hungary']
+       'https://en.wikipedia.org/wiki/Hungary'
+       ]
 
-mother_inverted_index, tfidf = driver(urls)
+def query_vector(words, idf):
+	qv = [0]*len(idf)
+	i = 0
+	for key in idf:
+		if key not in words:
+			i+=1
+		else:
+			qv[i] = idf[key]
+	return qv
+
+def get_relevant_urls(words, mother_inverted_index):
+	result= set()
+	position = []
+	for elem in words:
+		if elem not in mother_inverted_index.keys():
+			continue
+		else:
+			for i in range(len(mother_inverted_index[elem])):
+				position.append([elem, mother_inverted_index[elem]])
+				result.add(urls[mother_inverted_index[elem][i][0]])
+	return result
+
 query = input("Enter query: ")
-result= set()
-position = []
-
+mother_inverted_index, idf, map_tfidf_url = driver(urls)
 words=query.split()
-for elem in words:
-	if elem not in mother_inverted_index.keys():
-		continue
-	else:
-		for i in range(len(mother_inverted_index[elem])):
-			position.append([elem, mother_inverted_index[elem]])
-			result.add(urls[mother_inverted_index[elem][i][0]])
-
-print(result)
-
+urls = get_relevant_urls(words, mother_inverted_index)
+qv = query_vector(words, idf)
+n_qv = normalize(qv)
+import pdb; pdb.set_trace()
